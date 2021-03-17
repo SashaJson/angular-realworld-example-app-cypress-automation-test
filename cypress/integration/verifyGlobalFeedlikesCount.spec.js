56c435d9aefd308bf146f9f7c3d5e64c52a7bb7b
SashaJson <sashamiller666@gmail.com>
2021-03-18 01:06:33 +0200

    added new test "verify global feed likes count"

---
+++ cypress/integration/verifyGlobalFeedlikesCount.spec.js
@@ -0,0 +1,32 @@
+/// <reference> types="cypress" />
+
+describe('Testing verify global feed likes count', () => {
+
+    before('Login to the app', () => {
+        cy.loginToApplication();
+    });
+
+    it('Example first test', () => {
+
+        cy.intercept('GET', '**/articles/feed*', {"articles": [], "articlesCount": 0});
+        cy.intercept('GET', '**/articles*', {fixture: 'articles.json'});
+
+        cy.contains('Global Feed').click();
+        cy.get('app-article-list button').then(listOfButtons => {
+            expect(listOfButtons[0]).to.contain('1');
+            expect(listOfButtons[1]).to.contain('5');
+        });
+
+        cy.fixture('articles').then(file => {
+            const articleLink = file.articles[1].slug;
+            cy.intercept('POST', '**/articles/' + articleLink + '/favorite', file);
+        });
+
+        cy.get('app-article-list button')
+            .eq(1)
+            .click()
+            .should('contain', '6');
+
+    });
+
+});
